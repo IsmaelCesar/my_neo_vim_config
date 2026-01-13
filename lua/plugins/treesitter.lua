35ba9afccbcea41dbbb84cddcf2bf5eef20f182c
IsmaelCesar <leamscesar@gmail.com>
2026-01-13 17:22:14 -0300

    adjusting treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,4 +2,26 @@ return {
   'nvim-treesitter/nvim-treesitter',
   lazy = false,
   build = ':TSUpdate',
+  enabled = true,
+  config = function()
+    require'nvim-treesitter'.setup {
+
+      ensure_installed = {
+        "rust", 
+        "python",
+        "c",
+        "cpp",
+        "markdown",
+        "css",
+        "html",
+        "htmljango",
+        "javascript",
+        "java",
+        "yaml",
+        "json",
+      }
+  }
+  
+  end
+
 }
