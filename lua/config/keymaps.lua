87d91c2ba3054d246ce75719c42e1cac5a20f42a
IsmaelCesar <leamscesar@gmail.com>
2026-01-15 19:50:15 -0300

    Lazy menu

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,4 +4,5 @@ vim.keymap.set('n', '<space>bd', '<cmd>bdelete #<cr>', {desc='deleting buffer'})
 vim.keymap.set('n', '<S-h>', '<cmd>bp<cr>', {desc='Buffer Next'})
 vim.keymap.set('n', '<S-l>', '<cmd>bn<cr>', {desc='Buffer Previous'})
 vim.keymap.set('n', '<space>bp', '<cmd>BufferLineTogglePin<cr>', {desc='BufferLine Toggle Pin'})
+vim.keymap.set('n', '<space>L', '<cmd>Lazy<cr>', { desc='Lazy menu' })
 
