32eea144f556af7ac8ebb3fea43e98f9c4f3f411
IsmaelCesar <leamscesar@gmail.com>
2026-01-23 13:44:57 -0300

    Lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,4 +7,5 @@ vim.keymap.set('n', '<S-h>', '<cmd>bp<cr>', {desc='Buffer Next'})
 vim.keymap.set('n', '<S-l>', '<cmd>bn<cr>', {desc='Buffer Previous'})
 vim.keymap.set('n', '<space>bp', '<cmd>BufferLineTogglePin<cr>', {desc='BufferLine Toggle Pin'})
 vim.keymap.set('n', '<space>L', '<cmd>Lazy<cr>', { desc='Lazy menu' })
-
+vim.keymap.set('n', '<space>gt' ,function()  Snacks.picker.lsp_definitions() end, { desc='Snacks [picker] go to definition' })
+vim.keymap.set('n', '<space>gI', function() Snacks.picker.lsp_implementations() end , {desc='Go to implementation'})
