3775f36de5253411356024ad08a3023154e70e3b
IsmaelCesar <leamscesar@gmail.com>
2026-05-23 17:25:56 -0300

    updating snacks.lua plugins

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -9,7 +9,10 @@ return {
     -- refer to the configuration section below
     bigfile = { enabled = true },
     dashboard = { enabled = true },
-    explorer = { enabled = true },
+    explorer = { 
+      replace_netrw = true, -- replace netrw with snacks explorer
+      enabled = true,
+    },
     indent = { enabled = true },
     input = { enabled = true },
     picker = { enabled = true },
